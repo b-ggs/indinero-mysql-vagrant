2fcb071af9dd74008b20b5172aff8e8b250377eb
boggs <hello@boggs.xyz>
2017-06-02 21:11:56 +0800

    - Update provision to install MySQL 5.6
    - Remove comments from my.cnf

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 sudo apt-get update
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
+sudo apt-get -y install libaio1
+
+tar -xvf /vagrant/external/mysql-server_5.6.36-1ubuntu14.04_amd64.deb-bundle.tar -C /vagrant/external
+
+sudo dpkg -i /vagrant/external/mysql-common_5.6.36-1ubuntu14.04_amd64.deb
+sudo dpkg -i /vagrant/external/mysql-community-client_5.6.36-1ubuntu14.04_amd64.deb
+sudo dpkg -i /vagrant/external/mysql-client_5.6.36-1ubuntu14.04_amd64.deb
+sudo dpkg -i /vagrant/external/libmysqlclient18_5.6.36-1ubuntu14.04_amd64.deb
+
+sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password password'
+sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password password'
+sudo dpkg-preconfigure /vagrant/external/mysql-community-server_5.6.36-1ubuntu14.04_amd64.deb
+sudo dpkg -i /vagrant/external/mysql-community-server_5.6.36-1ubuntu14.04_amd64.deb
 
 # TODO: link this instead?
 sudo cp /vagrant/configs/my.cnf /etc/mysql 
